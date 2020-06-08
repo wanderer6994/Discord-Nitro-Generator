b1d493c08f7e63d9e5175b127fffbbf0b792d315
Stanley <66573599+Stanley-GF@users.noreply.github.com>
2020-06-08 19:23:03 +0200

    Add files via upload

---
+++ GenerateHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Colorful;
+
+namespace NitroChecker
+{
+	// Token: 0x02000003 RID: 3
+	internal class GenerateHelper
+	{
+		// Token: 0x06000009 RID: 9 RVA: 0x000026C0 File Offset: 0x000008C0
+		public static void Generate()
+		{
+			string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567891234567891234567891234567891234567891234567";
+			StreamWriter streamWriter = new StreamWriter("codes.txt");
+			try
+			{
+				Colorful.Console.WriteLine("Génération...");
+				for (int i = 0; i < GenerateHelper.amount; i++)
+				{
+					streamWriter.WriteLine(GenerateHelper.m2(16, charset));
+				}
+			}
+			finally
+			{
+				((IDisposable)streamWriter).Dispose();
+			}
+		}
+
+		// Token: 0x0600000A RID: 10 RVA: 0x0000272C File Offset: 0x0000092C
+		private static string m2(int length, string charset)
+		{
+			return new string(Enumerable.Repeat<string>(charset, length).Select(new Func<string, char>(GenerateHelper.m1)).ToArray<char>());
+		}
+
+		// Token: 0x0600000B RID: 11 RVA: 0x00002760 File Offset: 0x00000960
+		private static char m1(string string_0)
+		{
+			return string_0[GenerateHelper.random.Next(string_0.Length)];
+		}
+
+		// Token: 0x04000010 RID: 16
+		public static int amount;
+
+		// Token: 0x04000011 RID: 17
+		private static Random random = new Random();
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Colorful;
+
+namespace NitroChecker
+{
+	// Token: 0x02000004 RID: 4
+	internal class Program
+	{
+		// Token: 0x0600000E RID: 14 RVA: 0x00002794 File Offset: 0x00000994
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Program.Menu(args);
+		}
+
+		// Token: 0x0600000F RID: 15 RVA: 0x000027A0 File Offset: 0x000009A0
+		private static void Menu(string[] args)
+		{
+
+			Colorful.Console.WriteAscii("Stan Generator", Color.FromArgb(251, 51, 0));
+
+		
+
+			Colorful.Console.Title = (Colorful.Console.Title = "[Nitro Generator & Checker] | Make by Stanley#0001");
+			Thread.Sleep(250);
+			Colorful.Console.WriteLine("[1] Cree des codes nitros", Color.Orange);
+			Colorful.Console.WriteLine("[2] Check les codes nitros", Color.Orange);
+			Colorful.Console.WriteLine();
+			Colorful.Console.Write("", Color.DarkOrange);
+			string text = System.Console.ReadLine();
+			string text2 = text;
+			if (text2 != null)
+			{
+				if (!(text2 == "1"))
+				{
+					if (text2 == "2")
+					{
+						Colorful.Console.WriteLine();
+						Colorful.Console.Write("Combien de", Color.Orange);
+						Colorful.Console.Write("THREADS", Color.Orange);
+						Colorful.Console.Write(" voulez-vous utilisez", Color.Orange);
+						Colorful.Console.Write(": ", Color.Orange);
+						try
+						{
+							CheckerHelper.threads = int.Parse(Colorful.Console.ReadLine());
+						}
+						catch
+						{
+							CheckerHelper.threads = 100;
+						}
+						for (;;)
+						{
+							Colorful.Console.Write("> Quel type de ", Color.Orange);
+							Colorful.Console.Write("PROXIES ", Color.White);
+							Colorful.Console.Write("[HTTP, SOCKS4, SOCKS5, NO]", Color.DarkMagenta);
+							Colorful.Console.Write(": ", Color.DarkMagenta);
+							CheckerHelper.proxytype = Colorful.Console.ReadLine();
+							CheckerHelper.proxytype = CheckerHelper.proxytype.ToUpper();
+							bool flag = CheckerHelper.proxytype == "HTTP" || CheckerHelper.proxytype == "SOCKS4" || CheckerHelper.proxytype == "SOCKS5" || CheckerHelper.proxytype == "NO";
+							if (flag)
+							{
+								break;
+							}
+							Colorful.Console.Write("> Choisis un format valide.\n\n", Color.Red);
+							Thread.Sleep(2000);
+						}
+						Task.Factory.StartNew(delegate()
+						{
+							CheckerHelper.UpdateTitle();
+						});
+						Colorful.Console.WriteLine();
+						CheckerHelper.accounts = new List<string>(File.ReadAllLines("codes.txt"));
+						CheckerHelper.LoadCodes();
+						Colorful.Console.Write("> ");
+						Colorful.Console.Write(CheckerHelper.total, Color.DarkMagenta);
+						Colorful.Console.WriteLine(" Codes nitros ajouté\n");
+						OpenFileDialog openFileDialog = new OpenFileDialog();
+						bool flag2 = CheckerHelper.proxytype != "NO";
+						if (flag2)
+						{
+							string fileName;
+							do
+							{
+								Colorful.Console.WriteLine("Choisissez vos proxys", Color.DarkMagenta);
+								Thread.Sleep(500);
+								openFileDialog.Title = "Select Proxy List";
+								openFileDialog.DefaultExt = "txt";
+								openFileDialog.Filter = "Text files|*.txt";
+								openFileDialog.RestoreDirectory = true;
+								openFileDialog.ShowDialog();
+								fileName = openFileDialog.FileName;
+							}
+							while (!File.Exists(fileName));
+							CheckerHelper.proxies = new List<string>(File.ReadAllLines(fileName));
+							CheckerHelper.LoadProxies(fileName);
+							Colorful.Console.Write("> ");
+							Colorful.Console.Write(CheckerHelper.proxytotal, Color.DarkMagenta);
+							Colorful.Console.WriteLine(" Proxies ajouté\n");
+						}
+						for (int i = 1; i <= CheckerHelper.threads; i++)
+						{
+							new Thread(new ThreadStart(CheckerHelper.Check)).Start();
+						}
+						Colorful.Console.ReadLine();
+						Environment.Exit(0);
+					}
+				}
+				else
+				{
+					Colorful.Console.WriteLine();
+					Colorful.Console.Write("Combien de ", Color.Orange);
+					Colorful.Console.Write("CODES", Color.Orange);
+					Colorful.Console.Write(" voulez vous générez", Color.Orange);
+					Colorful.Console.Write(": ", Color.Orange);
+					try
+					{
+						GenerateHelper.amount = int.Parse(Colorful.Console.ReadLine());
+					}
+					catch
+					{
+						GenerateHelper.amount = 50000;
+					}
+					Task.Factory.StartNew(delegate()
+					{
+						GenerateHelper.Generate();
+					}).Wait();
+					Colorful.Console.WriteLine("Générez avec succès, retour au hub...", Color.Green);
+					Thread.Sleep(2000);
+					Program.Main(args);
+					System.Console.ReadLine();
+				}
+			}
+		}
+	}
+}
